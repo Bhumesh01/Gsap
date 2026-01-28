5909d5143ded15c21162ae5f01546ffbfda17a29
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-28 21:32:32 +0530

    basic

---
+++ tut-1/script.js
@@ -0,0 +1,14 @@
+// entrance animation
+gsap.to(".card", {
+  opacity: 1,
+  duration: 5,
+  scale: 1,
+  onComplete: ()=>{
+    gsap.to('.card', {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5
+    })
+  }
+});
