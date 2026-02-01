4cf47b4b24d6b39a474032be1a16890dafc87cb0
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-02-01 21:58:10 +0530

    back.out

--- tut-4/src/main.js
+++ tut-4/src/main.js
@@ -39,3 +39,27 @@ btn.addEventListener("mouseleave", ()=>{
 btn.addEventListener("click", ()=>{
   window.scrollTo(0,0);
 })
+
+const tabs = document.querySelectorAll(".link");
+const indicator = document.querySelector(".indicator");
+const tabRow = document.querySelector(".tabs");
+const updateIndicator = (target)=>{
+  const tabBounds = target.getBoundingClientRect();
+  const rowBounds = tabRow.getBoundingClientRect();
+  const width = tabBounds.width;
+  const offset = tabBounds.left - rowBounds.left;
+  gsap.to(indicator, {
+    x: offset,
+    width: width,
+    ease: 'back.out(1.7)'
+  })
+}
+const activeTab = document.querySelector(".link.active");
+updateIndicator(activeTab);
+tabs.forEach((tab)=>{
+  tab.addEventListener('click', ()=>{
+    tabs.forEach((t)=>t.classList.remove('active'));
+    tab.classList.add('active');
+    updateIndicator(tab);
+  })
+})
