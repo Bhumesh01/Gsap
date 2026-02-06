ee93511f726daee7125f82c66161167df99e6c48
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-02-06 19:58:38 +0530

    gsap.set

--- tut-6/src/main.js
+++ tut-6/src/main.js
@@ -12,3 +12,18 @@ pulses.forEach((pulse, index)=>{
     delay: index*0.1
   })
 })
+gsap.set('.gsap-box', {
+  scale: 0.5,
+  opacity: 0,
+  y: -100,
+  x: -100
+})
+gsap.to('.gsap-box', {
+  delay: 0.5,
+  opacity: 1,
+  scale: 1,
+  y: 0,
+  x: 0,
+  duration: 0.5,
+  ease: 'expo.out'
+})
